df52143adf80391297ee3e503c510a2b2b3a5e4c
AmirHossein Parhizgar <amparhizgar@gmail.com>
2022-05-26 18:09:04 +0430

    improve widget

--- app/src/main/java/com/amirhparhizgar/utdiningwidget/DiningWidget.kt
+++ app/src/main/java/com/amirhparhizgar/utdiningwidget/DiningWidget.kt
@@ -1,18 +1,25 @@
 package com.amirhparhizgar.utdiningwidget
 
 import android.content.Context
+import android.content.Intent
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.LocalContext
 import androidx.glance.action.ActionParameters
+import androidx.glance.action.actionParametersOf
+import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.action.ActionCallback
-import androidx.glance.layout.Column
-import androidx.glance.layout.Row
+import androidx.glance.appwidget.action.actionRunCallback
+import androidx.glance.appwidget.cornerRadius
+import androidx.glance.background
+import androidx.glance.layout.*
 import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
@@ -54,6 +61,10 @@ class DiningWidget : GlanceAppWidget() {
         )
     }
 
+    val callback = actionRunCallback<WidgetClickAction>(
+        parameters = actionParametersOf()
+    )
+
     @Composable
     fun Widget(
         context: Context,
@@ -61,8 +72,12 @@ class DiningWidget : GlanceAppWidget() {
         list: List<Item>,
         modifier: GlanceModifier
     ) {
-        Column {
-            Text(text = today)
+        Column(
+            modifier = GlanceModifier.fillMaxWidth().cornerRadius(8.dp).clickable(callback)
+                .background(Color(0f, 0f, 0f, 0.15f)).padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = today, modifier = GlanceModifier.clickable(callback))
             list.forEach {
                 FoodItem(it)
             }
@@ -73,6 +88,7 @@ class DiningWidget : GlanceAppWidget() {
     fun FoodItem(item: Item) {
         Row {
             Text(
+                modifier = GlanceModifier.clickable(callback),
                 text = item.meal.trim() + ": " + item.foodName.trim(),
                 style = TextStyle(
                     fontWeight = FontWeight.Bold,
@@ -86,13 +102,15 @@ class DiningWidget : GlanceAppWidget() {
     }
 }
 
-class AddWaterClickAction : ActionCallback {
+class WidgetClickAction : ActionCallback {
     override suspend fun onAction(
         context: Context,
         glanceId: GlanceId,
         parameters: ActionParameters
     ) {
-        DiningWidget().update(context, glanceId)
+        val intent = Intent(context, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        context.startActivity(intent)
     }
 }
 
