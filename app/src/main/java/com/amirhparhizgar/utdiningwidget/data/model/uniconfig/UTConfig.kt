ad2a7eb5d7b604d09a34e68360837433379795d7
Amir Hossein Parhizgar <amparhizgar@gmail.com>
2023-10-19 01:07:22 +0330

    support new login page

--- app/src/main/java/com/amirhparhizgar/utdiningwidget/data/model/uniconfig/UTConfig.kt
+++ app/src/main/java/com/amirhparhizgar/utdiningwidget/data/model/uniconfig/UTConfig.kt
@@ -6,6 +6,6 @@ package com.amirhparhizgar.utdiningwidget.data.model.uniconfig
 data class UTConfig(
     override val loginURL: String = "https://dining2.ut.ac.ir",
     override val reservesURL: String = "https://dining2.ut.ac.ir/Reserves",
-    override val usernameField: String = "username",
-    override val passwordField: String = "password"
+    override val usernameField: String = "Username",
+    override val passwordField: String = "Password"
 ): UniConfig
